85d8919bc66aa0591237ca9a21ee907da21dff87
HeejoSuh <15654639+HeejoSuh@users.noreply.github.com>
2018-04-05 14:06:12 -0400

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -9,27 +9,21 @@
 
 //Jet is the sub-class of Vehicle
 public class Truck extends Vehicle {
-	//Jet inherits all of Airplane's behaviour to use for its own
+	//class for truck
 	
 	public static final double MULTIPLIER = 0.4; 
-	//subclass can add its own variables to the ones that it inherits from Airplane 
 	
 	public Truck(String plate, String colour) throws Exception {
-		super(plate, colour); //refers to class this class has inherited behaviour from
-		//this calls the constructor of Airplane
+		super(plate, colour); 
 		super.setNumberOfDoors(2);
 		
 	}
 
 	public void Accelerate(double speed) {
-		//subclass can call superclass's methods, as well as changing its behaviour(OVERRIDING).
 		super.Accelerate(speed*MULTIPLIER);
 	}
 	
-	public void Deccelerate(double speed) {
-		super.Decelerate(speed*MULTIPLIER); //can also call it super.getSpeed()
+	public void Decelerate(double speed) {
+		super.Decelerate(speed*MULTIPLIER); 
 	}
-	
-	//eventhough not written
-	//getSpeed() can be used for Jet since it has inherited it from Airplane
 }
