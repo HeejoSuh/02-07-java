322b174563005f6ac701a09df76714e15f4bc8f3
HeejoSuh <15654639+HeejoSuh@users.noreply.github.com>
2018-04-06 13:19:50 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -22,35 +22,18 @@ public class Main {
 		//Bike 1
 		System.out.println("Bike 1-----------");
 		//instantiate new vehicle
-		Bike Bike1 = new Bike("Plate", "Orange");
-		System.out.println("Number of doors: "+Bike1.getNumberOfDoors());
-		System.out.println("Speed: "+Bike1.getSpeed());
-		Bike1.Accelerate(5.0);
-		System.out.println("Speed: "+Bike1.getSpeed());
-		Bike1.Accelerate(10);
-		System.out.println("Speed: "+Bike1.getSpeed());
+		Bike Bike1 = new Bike("Yellow");
 		Bike1.Accelerate(100);
-		System.out.println("Speed: "+Bike1.getSpeed());
-		Bike1.Decelerate(1);
-		System.out.println("Speed: "+Bike1.getSpeed());
+		System.out.println("Accelerated 100");
 		System.out.println(Bike1.All());
-		
 
 		//Truck 2
 		System.out.println("\nTruck 2-----------");
 		//instantiate new vehicle
 		//Vehicle vehicle2 = new Vehicle();
-		Truck Truck2 = new Truck("POI0987", "Black");
-		System.out.println("Number of doors: "+Truck2.getNumberOfDoors());
-		System.out.println("Speed: "+Truck2.getSpeed());
-		Truck2.Accelerate(5.0);
-		System.out.println("Speed: "+Truck2.getSpeed());
-		Truck2.Accelerate(10.2);
-		System.out.println("Speed: "+Truck2.getSpeed());
-		Truck2.Accelerate(300);
-		System.out.println("Speed: "+Truck2.getSpeed());
-		Truck2.Decelerate(100.0);
-		System.out.println("Speed: "+Truck2.getSpeed());
+		Truck Truck2 = new Truck("ABC1234", "Grey");
+		Truck2.Accelerate(100);
+		System.out.println("Accelerated 100");
 		System.out.println(Truck2.All());
 	}
 }
